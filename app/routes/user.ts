2532e26bced0923b18d7901ca73f771fbab4dab9
leliso <lelisoagegneh491@gmail.com>
2023-08-07 15:23:04 +0300

    add error handler

--- app/routes/user.ts
+++ app/routes/user.ts
@@ -1,11 +1,29 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { getUsers } from "~/server/getUsers";
+import { errorHandler } from "~/utils/response/errorHandler";
+import { Response } from "~/utils/response/response";
+import { validate } from "~/utils/response/validate";
+import { userSchema } from "~/utils/schema/user";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   try {
-    return await getUsers(request);
+    const users = await getUsers(request);
+    return json(Response({ data: users }), { status: 201 });
   } catch (error) {
-    console.log(error);
-    return error;
+    return errorHandler(error);
+  }
+};
+
+export const action: ActionFunction = async (args) => {
+  try {
+    const formData = Object.fromEntries(await args.request.formData());
+    const { data } = validate(formData, userSchema);
+    return json(
+      Response({ message: "User Successfully created !", data: data }),
+      { status: 201 }
+    );
+  } catch (error) {
+    return errorHandler(error);
   }
 };
