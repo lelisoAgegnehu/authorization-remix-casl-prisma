368dc0cc0bea21b11b60782afc7df564e0eb88d0
leliso <lelisoagegneh491@gmail.com>
2023-08-07 15:23:30 +0300

    add ability

--- app/server/deleteUser.ts
+++ app/server/deleteUser.ts
@@ -1,8 +1,10 @@
+import { subject } from "@casl/ability";
 import { softDeleteUser } from "~/dataAccess/user/softDeleteUser";
-import { userAbility } from "~/utils/userAbility";
+import { userAbility } from "~/utils/casl/userAbility";
 
 export const deleteUser = async (request, userId) => {
   const ability = await userAbility("7ab1ff7d-e916-49ab-bc32-d2e897e62b7d");
+  // console.log(ability.can("update", subject("User", { id: "rr" })));
   const users = await softDeleteUser(userId, ability);
   return users;
 };
--- app/server/getUsers.ts
+++ app/server/getUsers.ts
@@ -1,5 +1,5 @@
 import { fetchUsers } from "~/dataAccess/user/fetchUsers";
-import { userAbility } from "~/utils/userAbility";
+import { userAbility } from "~/utils/casl/userAbility";
 
 export const getUsers = async (request) => {
   const ability = await userAbility("7ab1ff7d-e916-49ab-bc32-d2e897e62b7d");
